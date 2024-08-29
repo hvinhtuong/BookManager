786cbf65aa398c8842a66d2193f9dd9274b55b86
Rains97 <hvinhtuong@gmail.com>
2024-08-29 09:53:50 +0700

    Book manager

---
+++ src/main/java/com/example/bookmanager/entity/BookEntity.java
@@ -0,0 +1,32 @@
+package com.example.bookmanager.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookEntity extends BaseEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private int sold;
+
+    @Column(nullable = false)
+    private LocalDate entryDate;
+}
